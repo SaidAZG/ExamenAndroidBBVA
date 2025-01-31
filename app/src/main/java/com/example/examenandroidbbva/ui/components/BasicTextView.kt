527c58908d8ea9b2fbf4fd28bcc18549079501f7
Said Zúñiga <said.zuniga@sumamexico.com>
2025-01-31 00:23:24 -0600

    style(Component) : Componente TextView base

---
+++ app/src/main/java/com/example/examenandroidbbva/ui/components/BasicTextView.kt
@@ -0,0 +1,27 @@
+package com.example.examenandroidbbva.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.example.examenandroidbbva.ui.theme.DarkBlue
+
+@Preview(showBackground = true)
+@Composable
+fun BasicTextView(
+    text: String = "Hola",
+    fontSize: TextUnit = 18.sp,
+    color: Color = DarkBlue,
+    fontWeight: FontWeight = FontWeight.Normal,
+    allCaps: Boolean = false
+){
+    Text(
+        text = if (allCaps) text.uppercase() else text,
+        color = color,
+        fontWeight = fontWeight,
+        fontSize = fontSize
+    )
+}
