0ab7bae3fd5005ac00eefcef1521492293aa68b4
Said Zúñiga <said.zuniga@sumamexico.com>
2025-01-30 22:08:30 -0600

    style(Component) : Botón base

---
+++ app/src/main/java/com/example/examenandroidbbva/ui/components/BasicButton.kt
@@ -0,0 +1,33 @@
+package com.example.examenandroidbbva.ui.components
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.examenandroidbbva.ui.theme.Blue
+import com.example.examenandroidbbva.ui.theme.White
+
+@Preview
+@Composable
+fun BasicButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: String = "Hola",
+    onClick: () -> Unit = {},
+){
+    Button(
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Blue,
+        ),
+        onClick = { /*TODO*/ },
+        enabled = enabled
+    ){
+        Text(
+            text = text.uppercase(),
+            color = White
+        )
+    }
+}
