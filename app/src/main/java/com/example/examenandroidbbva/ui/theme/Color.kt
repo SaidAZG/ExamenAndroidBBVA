f153182250c2f402a30c0d6081ab7972900f62c1
Said Zúñiga <said.zuniga@sumamexico.com>
2025-01-30 22:05:59 -0600

    style(Color) : Colores base

--- app/src/main/java/com/example/examenandroidbbva/ui/theme/Color.kt
+++ app/src/main/java/com/example/examenandroidbbva/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkBlue = Color(0xFF19213A)
+val NavyBlue = Color(0xFF1A2E54)
+val Blue = Color(0xFF0F3D74)
+val LightBlue = Color(0xFF13599B)
+
+val BrigthPink = Color(0xFFEE3A6A)
