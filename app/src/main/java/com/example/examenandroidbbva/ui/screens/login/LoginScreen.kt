0fb9cceee537dcb79ae330b93567fb005c09fe66
Said Zúñiga <said.zuniga@sumamexico.com>
2025-01-31 00:57:28 -0600

    style(Login) : Diseño Login

--- app/src/main/java/com/example/examenandroidbbva/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/examenandroidbbva/ui/screens/login/LoginScreen.kt
@@ -3,6 +3,7 @@ package com.example.examenandroidbbva.ui.screens.login
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.examenandroidbbva.ui.components.BasicButton
 import com.example.examenandroidbbva.ui.components.BasicTextField
@@ -43,6 +45,7 @@ fun LoginScreen(
             onValueChange = { viewModel.onEmailChanged(it) },
         )
         BasicTextField(
+            modifier = Modifier.padding(vertical = 12.dp),
             value = state.password,
             label = "Contraseña",
             visualTransformation = PasswordVisualTransformation(),
