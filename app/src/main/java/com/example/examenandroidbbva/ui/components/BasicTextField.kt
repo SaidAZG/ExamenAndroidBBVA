e38b9f773b67da0b65a2edeac3996811e134bcd8
Said Zúñiga <said.zuniga@sumamexico.com>
2025-01-30 22:28:12 -0600

    style(Component) : Componente TextField base

---
+++ app/src/main/java/com/example/examenandroidbbva/ui/components/BasicTextField.kt
@@ -0,0 +1,44 @@
+package com.example.examenandroidbbva.ui.components
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.examenandroidbbva.ui.theme.DarkBlue
+import com.example.examenandroidbbva.ui.theme.Transparent
+import com.example.examenandroidbbva.ui.theme.White
+
+@Preview
+@Composable
+fun BasicTextField(
+    modifier: Modifier = Modifier,
+    value: String = "",
+    label: String = "",
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onValueChange: (String) -> Unit = {},
+){
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        label = { Text(label) },
+        onValueChange = onValueChange,
+        visualTransformation = visualTransformation,
+        textStyle = TextStyle(color = DarkBlue),
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = DarkBlue,
+            unfocusedTextColor = DarkBlue,
+            focusedContainerColor = White,
+            unfocusedContainerColor = White,
+            focusedIndicatorColor = Transparent,
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text
+        )
+    )
+}
